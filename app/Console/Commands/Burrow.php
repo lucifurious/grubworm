748705c2fc09c4bd4db64893a9a8c5cbb07ac691
Jerry Ablan <jerryablan@dreamfactory.com>
2015-05-17 16:53:11 -0400

    Composer dependencies updated.

--- app/Console/Commands/Burrow.php
+++ app/Console/Commands/Burrow.php
@@ -3,12 +3,30 @@
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Burrows into a database to discover all the goodies within
  */
 class Burrow extends Command
 {
+    //******************************************************************************
+    //* Constants
+    //******************************************************************************
+
+    /**
+     * @type string
+     */
+    const VERSION = 'v1.0.x-dev';
+    /**
+     * @type string
+     */
+    const VERSION_DATE = '2015-05-17';
+    /**
+     * @type string Default output path of this command relative to app path
+     */
+    const DEFAULT_OUTPUT_PATH = 'database/grubworm';
+
     //******************************************************************************
     //* Members
     //******************************************************************************
@@ -17,10 +35,40 @@ class Burrow extends Command
     protected $name = 'grubworm:burrow';
     /**  @var string The console command description */
     protected $description = 'Burrow into a database and discover all its goodies';
+    /**
+     * @type int
+     */
+    protected $_verbosity;
+    /**
+     * @type OutputInterface
+     */
+    protected $_output;
+    /**
+     * @type string Destination path of output. Defaults to app/database/grubworm/
+     */
+    protected $_destination;
 
     //******************************************************************************
     //* Methods
     //******************************************************************************
+
+    /**
+     * Echo's the intro header
+     */
+    protected function _intro()
+    {
+        $_version = static::VERSION . ' (' . static::VERSION_DATE . ')';
+        ( ( $_year = date( 'Y' ) ) > 2015 ) && $_year = '2015-' . $_year;
+
+        $_intro = <<<TEXT
+DreamFactory Grubworm {$_version}
+Copyright (C) {$_year} DreamFactory Software, Inc.  All rights reserved.
+This software comes with NO WARRANTY: see the file .dreamfactory.php for details.
+
+TEXT;
+
+        $this->_writeln( $_intro );
+    }
 
     /**
      * Handle the command
@@ -29,9 +77,18 @@ class Burrow extends Command
      */
     public function fire()
     {
-        $_database = $this->argument( 'database' );
+        $this->_output = $this->getOutput();
+        $this->_verbosity = $this->_output->getVerbosity();
 
-        empty( $_database ) && $this->output->writeln( 'grubworm: using default database' );
+        $this->_intro();
+
+        if ( null === ( $_path = $this->option( 'output-path' ) ) )
+        {
+            $_path = app_path() . DIRECTORY_SEPARATOR . ltrim( static::DEFAULT_OUTPUT_PATH, DIRECTORY_SEPARATOR );
+        }
+
+        $_database = $this->argument( 'database' );
+        empty( $_database ) && $this->_v( '* using <info>default</info> database' );
 
         try
         {
@@ -39,10 +96,11 @@ class Burrow extends Command
         }
         catch ( \Exception $_ex )
         {
-            throw new \InvalidArgumentException( 'The database "' . $_database ?: \DB::getDatabaseName() . '" is invalid.' );
+            throw new \InvalidArgumentException( 'The database "' . ( $_database ?: 'default' ) . '" is invalid.' );
         }
 
-        $this->output->writeln( 'grubworm: connected to database <info>' . $_database ?: \DB::getDatabaseName() . '</info>.' );
+        $_database = $_database ?: \DB::getDatabaseName();
+        $this->_v( '* connected to database <info>' . $_database . '</info>.' );
 
         $_tablesWanted = $this->option( 'tables' );
 
@@ -52,19 +110,24 @@ class Burrow extends Command
             $_tablesWanted = empty( $_list ) ? false : $_tablesWanted = $_list;
         }
 
-        $_sm = $_db->getDoctrineConnection()->getSchemaManager();
-        $_dbTables = $_sm->listTableNames();
-        $this->output->writeln( 'grubworm: found <info>' . count( $_dbTables ) . '</info> tables.' . print_r( $_dbTables, true ) );
+        !empty( $_tablesWanted ) &&
+        $this->_vv( '* there are ' . count( $_tablesWanted ) . ' table(s) to be scanned.' ) ||
+        $this->_vv( '* all tables will be scanned.' );
 
-        foreach ( $_dbTables as $_table )
+        $_sm = $_db->getDoctrineConnection()->getSchemaManager();
+        $_tableNames = $_sm->listTableNames();
+
+        $this->_writeln( '* examining ' . count( $_tableNames ) . ' table(s)...' );
+
+        foreach ( $_tableNames as $_tableName )
         {
-            if ( $_tablesWanted && !in_array( $_table, $_tablesWanted ) )
+            if ( $_tablesWanted && !in_array( $_tableName, $_tablesWanted ) )
             {
-                $this->output->writeln( '<info>* Would SKIP table <warning>' . $_table . '</warning>.</info>' );
+                $this->_vvv( '  * SKIP table <comment>' . $_tableName . '</comment>.' );
                 continue;
             }
 
-            $this->output->writeln( '<success>* Would check table <warning>' . $_table . '</warning>.</success>' );
+            $this->_vv( '  * SCAN table <info>' . $_tableName . '</info>.' );
         }
     }
 
@@ -87,4 +150,51 @@ class Burrow extends Command
 
         return array_merge( parent::getArguments(), $_arguments );
     }
+
+    /**
+     * @param string|array $messages The message as an array of lines of a single string
+     * @param int          $type     The type of output (one of the OUTPUT constants)
+     *
+     * @throws \InvalidArgumentException When unknown output type is given
+     */
+    protected function _vvv( $messages, $type = OutputInterface::OUTPUT_NORMAL )
+    {
+        $this->_writeln( $messages, OutputInterface::VERBOSITY_DEBUG, $type );
+    }
+
+    /**
+     * @param string|array $messages The message as an array of lines of a single string
+     * @param int          $type     The type of output (one of the OUTPUT constants)
+     *
+     * @throws \InvalidArgumentException When unknown output type is given
+     */
+    protected function _vv( $messages, $type = OutputInterface::OUTPUT_NORMAL )
+    {
+        $this->_writeln( $messages, OutputInterface::VERBOSITY_VERY_VERBOSE, $type );
+    }
+
+    /**
+     * @param string|array $messages The message as an array of lines of a single string
+     * @param int          $type     The type of output (one of the OUTPUT constants)
+     *
+     * @throws \InvalidArgumentException When unknown output type is given
+     */
+    protected function _v( $messages, $type = OutputInterface::OUTPUT_NORMAL )
+    {
+        $this->_writeln( $messages, OutputInterface::VERBOSITY_VERBOSE, $type );
+    }
+
+    /**
+     * @param string|array $messages
+     * @param int          $level
+     * @param int          $type
+     */
+    protected function _writeln( $messages, $level = OutputInterface::VERBOSITY_NORMAL, $type = OutputInterface::OUTPUT_NORMAL )
+    {
+        if ( $level <= $this->_verbosity )
+        {
+            $this->_output->writeln( $messages, $type );
+        }
+    }
+
 }
